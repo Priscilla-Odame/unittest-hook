8aa11d8ab588de51bbf3f7bcd646b15e19c5d065
Priscilla Odame <42655518+Priscilla-Odame@users.noreply.github.com>
2020-11-20 12:03:42 +0000

    Update run-unittest.sh

--- run-unittest.sh
+++ run-unittest.sh
@@ -3,7 +3,7 @@
 echo 'Running tests'
 
 git stashi > /dev/null
-python3 manage.py test '*_test.py'
+python3 manage.py test '*test_.py'
 if [ $? -ne 0 ]; then
 	echo 'A unit test failed. Aborting commit'
 	echo 'Bypass with git commit --no-verify'
